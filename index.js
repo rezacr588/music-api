86eef94e6a2bbe44a057844616c9a828a2f67405
rezacr588 <rezacr588@gmail.com>
2020-11-23 23:31:10 +0330

    xcxvxvxxc

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const config = require("config");
 require("./apps/mongo")(config);
 const app = require("express")();
 require("./apps/middlewares")(app);
-app.listen(config.serverConfig.port, () => {
+app.listen(config.get("port"), () => {
   console.log(`Music API is running on ` + process.env.NODE_ENV);
 });
