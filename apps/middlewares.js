d23ca6ad2cc05524699225e64769b37abab7b19c
rezacr588 <rezacr588@gmail.com>
2020-11-23 23:37:08 +0330

    sadasdads

--- apps/middlewares.js
+++ apps/middlewares.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+const express = require("express");
 Joi.objectId = require("joi-objectid")(Joi);
 const bodyParser = require("body-parser");
 module.exports = function (app) {
@@ -7,5 +8,8 @@ module.exports = function (app) {
   app.use(bodyParser.json());
   app.use("/api", require("../src/routes/index"));
   app.use(require("../src/middlewares/error"));
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + "/public"));
+  app.get("/", function (req, res) {
+    res.send("mahmood");
+  });
 };
