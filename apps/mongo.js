ff759bd02ee60e8af10d7a97f036f9c5961cee27
rezacr588 <rezacr588@gmail.com>
2020-11-24 19:16:57 +0330

    Update mongo.js

--- apps/mongo.js
+++ apps/mongo.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
 module.exports = function (config) {
   console.log(config.dbConfig.connection);
-  mongoose
-    .connect(config.dbConfig.connection, config.dbConfig.options)
-    .then(() => {
-      console.log("db is connected");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  mongoose.connect(config.dbConfig.connection, config.dbConfig.options);
+  mongoose.connection.once("open", () => console.log("db is connected!"));
 };
