04da8ef4424e191db3b02ab0eab815eb4ac1afb8
rezacr588 <rezacr588@gmail.com>
2020-11-28 04:01:01 +0330

    upload part 1

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,2 +1,23 @@
-const AWS = require("aws-sdk");
-module.exports = (req, res, next) => {};
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const s3 = new aws.S3({
+  accessKeyId: "fc3e37e2-0f10-418e-a7e5-9f86e2a99730",
+  secretAccessKey:
+    "d55edbca2d3959129f512ed965523e71ecd551df62c909da3556d2c8f02c9812",
+  endpoint: "https://s3.ir-thr-at1.arvanstorage.com",
+  s3ForcePathStyle: true,
+});
+module.exports = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "musics",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
--- src/routes/artists.js
+++ src/routes/artists.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { validation, deleteSchema } = require("../middlewares/joiValidation");
 const paginate = require("../middlewares/paginate");
+const upload = require("../middlewares/upload");
 const { joiSchema } = require("../models/artist");
 const {
   create,
@@ -10,11 +11,10 @@ const {
 } = require("../controllers/artistController");
 const { protect, authorize } = require("../middlewares/auth");
 const router = express.Router();
-
 router
   .route("/")
   .get(paginate, index)
-  .post(protect, authorize(10), validation(joiSchema), create)
+  .post(protect, authorize(10), upload.single('cover'), validation(joiSchema), create)
   .patch(protect, authorize(10), validation(joiSchema), patch)
   .delete(protect, authorize(10), validation(deleteSchema), handleDelete);
 module.exports = router;
