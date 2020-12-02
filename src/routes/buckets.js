9581ad305b3d7ced24f6c8e794b2046a290f6832
rezacr588 <rezacr588@gmail.com>
2020-12-02 06:30:39 +0330

    Update buckets.js

--- src/routes/buckets.js
+++ src/routes/buckets.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { protect, authorize } = require('../middlewares/auth');
 const { index, create, remove } = require('../controllers/bucketController');
-express
-  .Router()
+const router = express.Router();
+router
   .route('/')
   .get(protect, authorize(10), index)
   .post(protect, authorize(10), create)
