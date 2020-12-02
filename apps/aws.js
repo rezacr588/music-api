e49e0fd248e68f8255c31613fa6349cfff984150
rezacr588 <rezacr588@gmail.com>
2020-12-02 07:18:49 +0330

    sadsada

--- apps/aws.js
+++ apps/aws.js
@@ -5,6 +5,6 @@ const s3 = new aws.S3({
   secretAccessKey: config.get('secretAccessKey'),
   endpoint: config.get('endpoint'),
   s3ForcePathStyle: true,
-  region: 'REGION',
+  region: 'ir-thr-at1',
 });
 module.exports = s3;
