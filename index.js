213ae7bc1a331cd0a0a3aa1100a025b4b675d134
rezacr588 <rezacr588@gmail.com>
2020-12-03 13:12:37 +0330

    dsadsada

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// const cluster = require('cluster');
 const config = require('config');
 const app = require('./apps/app');
-// if (cluster.isMaster) {
-//   cluster.fork();
-//   cluster.fork();
-// } else {
 require('./apps/middlewares')(app);
 app.listen(config.get('port'));
-// }
